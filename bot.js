9ebbefd8b96f964abedaeffc9602a09af98ae686
eddie cianci <defeated2k4@gmail.com>
2015-12-31 13:10:23 -0500

    use Heroku web dyno for free/hobby tier. fixes "failed to bind to $PORT".

--- bot.js
+++ bot.js
@@ -10,18 +10,26 @@ if (!process.env.MONGO_URL) {
   process.exit(1);
 }
 
-var Botkit = require('botkit');
+// http port number
+if (!process.env.PORT) {
+  console.log('Error: missing PORT environment variable.');
+  process.exit(1);
+}
 
+// configure storage
 var mongo_storage = require('botkit/lib/storage/mongo_storage')({
   mongo_uri: process.env.MONGO_URL
 });
 
+// create bot
+var Botkit = require('botkit');
 var controller = Botkit.slackbot({
  debug: true,
  log: true,
  storage: mongo_storage
 });
 
+// connect bot to chat
 controller.spawn({
   token: process.env.SLACK_TOKEN
 }).startRTM(function(err) {
@@ -30,6 +38,12 @@ controller.spawn({
   }
 });
 
+// simple listener (reply to IM containing hello or hi.)
 controller.hears(['hello','hi'],'direct_message',function(bot,message) {
   bot.reply(message,"Hello.");
-})
+});
+
+// HACK: use Heroku web dyno (avoid "Web process failed to bind to $PORT")
+controller.setupWebserver(process.env.PORT,function(err,webserver) {
+  // TODO: something userful here later?
+});
